24f6db61fc479a6490db58b276e04af9e08c6816
Fred <terrage.frederic@orange.fr>
2017-10-11 11:35:01 +0200

    Ajout de la class User.java

---
+++ user.java
@@ -0,0 +1,27 @@
+package testFred;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User {
+
+	
+	public ArrayList<Integer> getCode() {
+		
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		
+		char[] tab = str.toCharArray();
+		ArrayList<Integer> code = new ArrayList<Integer>();
+		code.add(Integer.parseInt(Character.toString(tab[0])));
+		code.add(Integer.parseInt(Character.toString(tab[1])));
+		code.add(Integer.parseInt(Character.toString(tab[2])));
+		code.add(Integer.parseInt(Character.toString(tab[3])));
+		
+		return code;
+
+	}
+	
+	
+}
+
