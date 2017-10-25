3b7fed719c8771f2e9809016413a9abfc22481aa
Fred <terrage.frederic@orange.fr>
2017-10-25 13:50:51 +0200

    classe duel pour le jeu recherche+/-

--- duel.java
+++ duel.java
@@ -1,60 +1,63 @@
-package testFred;
+package fr.projet3.jeux;
 
 import java.util.ArrayList;
 
 public class Duel {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Recherche +/-");
-		System.out.println("Mode duel");
+	
+	public Duel() {
 
 		UserDuel gamer1 = new UserDuel();
-		IADuel gamer2 = new IADuel();
+		IA gamer2 = new IA();
 
-		ArrayList<Integer> codeIA = gamer2.generateCode();
+		ArrayList<Integer> code = gamer2.generateCode();
+		
+		System.out.println("\nSaisir la combinaison à faire deviner à IA : ");
 		ArrayList<Integer> codeUserDuel = gamer1.getCode();
+		
+		System.out.print("Votre combinaison : ");
+		for (int i = 0; i < 4; i++){
+		System.out.print(codeUserDuel.get(i));
+		}
 		// nombre total d'essais
 		int nombreEssaisRestant = 10;
-		
+
+		ArrayList<Integer> propositionIA = null;
 		String resultUser = null;
 		String resultIA = null;
-		//ArrayList<Integer> proposition = null;
-		
+
 		// lancement du jeu
 		while (nombreEssaisRestant > 0) {
-			
-			System.out.println("\nsaisir la combinaison");
+
+			System.out.println("\nSaisir une proposition :");
 			ArrayList<Integer> propositionUserDuel = gamer1.getCode();
 
-			System.out.print("\nProposition : ");
+			System.out.print("\nProposition user : ");
 			for (int j = 0; j < 4; j++) {
 				System.out.print(propositionUserDuel.get(j));
 			}
 			System.out.print(" -> réponse : ");
 
-			// vérification de la proposition user par IA
+			// vérification de la proposition user
 			resultUser = "";
 			for (int i = 0; i < 4; i++) {
-				if (codeIA.get(i) == propositionUserDuel.get(i)) {
+²				if (code.get(i) == propositionUserDuel.get(i)) {
 					resultUser += "=";
-				} else if (codeIA.get(i) < propositionUserDuel.get(i)) {
+				} else if (code.get(i) < propositionUserDuel.get(i)) {
 					resultUser += "-";
 				} else
 					resultUser += "+";
 			}
 			System.out.println(resultUser);
-			
-			ArrayList<Integer> propositionIA = gamer2.generateCode();			
+
 			propositionIA = gamer2.generateCode(propositionIA, resultIA);
 			// vérification de la proposition IA
-			System.out.print("\nProposition : ");
-			for (int j = 0; j < 4; j++){
-				System.out.print(propositionIA.get(j));					
-				}
+			System.out.print("\nProposition IA : ");
+			for (int j = 0; j < 4; j++) {
+				System.out.print(propositionIA.get(j));
+			}
 			System.out.print(" -> réponse : ");
-			
-			resultIA = "";			
+
+			resultIA = "";
 			for (int i = 0; i < 4; i++) {
 				if (codeUserDuel.get(i) == propositionIA.get(i)) {
 					resultIA += "=";
@@ -65,17 +68,17 @@ public class Duel {
 			}
 			System.out.println(resultIA);
 
-			if (resultIA.equals("====")) 
-				System.out.println("c'est gagné");
-			else if (resultUser.equals("===="));
-				System.out.println("c'est gagné");
-			
+			if (resultIA.equals("====")) {
+				System.out.println("BRAVO IA a gagné !");
+				nombreEssaisRestant = 0;
+			} else if (resultUser.equals("====")) {
+				System.out.println("BRAVO vous avez gagné !");
 				nombreEssaisRestant = 0;
 			}
 			// Si combinaison trouvé fin, sinon relance 4 jusqu'à fin du nombre
 			// d'essai possible
 			nombreEssaisRestant--;
 		}
+	}
 }
-
 
