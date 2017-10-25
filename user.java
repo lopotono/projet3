0986b7d3f3287c92f04101f2fa22be9ca3a48505
Fred <terrage.frederic@orange.fr>
2017-10-25 13:55:19 +0200

    classe user.java

--- user.java
+++ user.java
@@ -1,27 +1,24 @@
-package testFred;
+package fr.projet3.jeux;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
 
-	
 	public ArrayList<Integer> getCode() {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String str = scanner.nextLine();
-		
+
 		char[] tab = str.toCharArray();
 		ArrayList<Integer> code = new ArrayList<Integer>();
 		code.add(Integer.parseInt(Character.toString(tab[0])));
 		code.add(Integer.parseInt(Character.toString(tab[1])));
 		code.add(Integer.parseInt(Character.toString(tab[2])));
 		code.add(Integer.parseInt(Character.toString(tab[3])));
-		
-		return code;
 
+		return code;
 	}
-	
-	
+
 }
 
