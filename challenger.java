8fd45ba2f5a1227ccf722f02f91147238559903a
Fred <terrage.frederic@orange.fr>
2017-11-08 16:05:07 +0100

    Modification de Challenger.java avec config nombreEssais

--- challenger.java
+++ challenger.java
@@ -1,6 +1,10 @@
+
 package fr.projet3.jeux;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Challenger {
 
@@ -12,13 +16,22 @@ public class Challenger {
 		// générer la combinaison
 		ArrayList<Integer> code = gamer2.generateCode();
 
-		// Déterminer le nombre limité d'essai
-		int nombreEssaisRestant = 10; // TODO : Récupérer cette valeur dans un
-										// fichier
-										// de configuration.
+		Properties properties = new Properties();
+
+		try {
+			FileInputStream in = new FileInputStream("D://workspace/fr.projet3.jeux/src/fr/projet3/jeux/config.properties");
+			properties.load(in);
+			in.close();
+		} catch (IOException e) {
+			System.out.println("Erreur");
+		}
+				
+		int nombreEssais = Integer.parseInt(properties.getProperty("nombreEssais"));
+		// TODO : Récupérer cette valeur dans un fichier de configuration.
+															
 		// Lancer le jeu
-		while (nombreEssaisRestant > 0) {
-			
+		while (nombreEssais > 0) {
+
 			System.out.println("\nSaisir une combinaison à 4 chiffres : ");
 			ArrayList<Integer> proposition = gamer1.getCode();
 
@@ -43,12 +56,10 @@ public class Challenger {
 			// Afficher le retour (combinaison trouvé ou non)
 			if (result.equals("====")) {
 				System.out.println("BRAVO c'est gagné !");
-				nombreEssaisRestant = 0;
-			}						
-			nombreEssaisRestant--;
-			
-			
+				nombreEssais = 0;
+			}
+			nombreEssais--;
+
 		}
 	}
 }
-
