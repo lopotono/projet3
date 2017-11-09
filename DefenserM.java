e7165bc1ad37d30f5bb370e99aa4cde97e0213b8
Fred <terrage.frederic@orange.fr>
2017-11-09 17:23:29 +0100

    Modification classe DefenserM.java

--- DefenserM.java
+++ DefenserM.java
@@ -1,8 +1,12 @@
 
 
+
 package fr.projet3.jeux;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import javax.naming.spi.DirStateFactory.Result;
 
@@ -17,30 +21,37 @@ public class DefenserM {
 		System.out.println("\nSaisir une combinaison à 4 chiffres : ");
 
 		ArrayList<Integer> code = gamer1.getCode();
+		
+		Properties properties = new Properties();
 
-		// nombre d'essais
-		int nombreEssais = 10;
-
+		try {
+			FileInputStream in = new FileInputStream("D://workspace/fr.projet3.jeux/src/fr/projet3/jeux/config.properties");
+			properties.load(in);
+			in.close();
+		} catch (IOException e) {
+			System.out.println("Erreur");
+		}
+		
+		int nombreEssaisM = Integer.parseInt(properties.getProperty("nombreEssaisM"));
+		// TODO : Récupérer cette valeur dans un fichier de configuration.
 		ArrayList<Integer> proposition = null;
-		int result = 0;
+		
 		// Lancement du jeu
-
-		while (nombreEssais > 0) {
+		while (nombreEssaisM > 0) {
 
 			int bienPlace = 0;
 			int present = 0;
 						
-			proposition = gamer2.generateCode(proposition, result);
+			proposition = gamer2.generateCode();
 			// Vérification de la proposition
 			ArrayList<Integer> temp = new ArrayList<Integer>();
-			// Etape 1 : parcourir la proposition
+			// Parcourir la proposition
 			for (int i = 0; i < 4; i++) {
 				int value = proposition.get(i);
-				// Etape 2 : si la proposition est bien placé, incrémenté bien
+				// Si la proposition est bien placé, incrémenté bien
 				// placé et ajouter la valeur dans temp
 				if (code.get(i) == value) {
 					bienPlace++;
-					temp.add(value);
 				} else if (code.contains(value)) {
 					int res = Helper.getNbValueInArray(value, code);
 					// Cas n°1 : il n'est pas bien placé
@@ -51,7 +62,6 @@ public class DefenserM {
 						present++;
 					}
 				}
-				result = 0;
 				System.out.print("\nProposition : ");
 				for (int j = 0; j < 4; j++) {
 					System.out.print(value);
@@ -61,9 +71,9 @@ public class DefenserM {
 
 				if (bienPlace == 4) {
 					System.out.println("\nBRAVO c'est gagné !");
-					nombreEssais = 0;
+					nombreEssaisM = 0;
 				}
-				nombreEssais--;
+				nombreEssaisM--;
 			}
 		}
 	}
